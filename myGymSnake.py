8445d88c908f3c7ede3fa3621b8d6a1831c48356
Jacco Steegman <jacc.stee@gmail.com>
2019-10-15 00:07:35 +0200

    added basic stats tracking

--- myGymSnake.py
+++ myGymSnake.py
@@ -9,18 +9,74 @@ from sneks.envs.snek import SingleSnek
 import time
 from tqdm import tqdm
 
+import pandas as pd
+import numpy as np
+
 #%%
-env = SingleSnek(obs_type='rgb', n_food=1, size=(32,32), dynamic_step_limit=100, step_limit=100000,render_zoom=25, add_walls=True)
+maxSnakeLife = 1000
+hunger = 200
+gameSize = 32
+env = SingleSnek(obs_type='rgb', n_food=20, size=(gameSize,gameSize), dynamic_step_limit=hunger,
+                 step_limit=maxSnakeLife + 1,render_zoom=50, add_walls=False)
+
+#%%
+rewards = list()
+totalreward = 0
+move_counts = list()
+move = dict()
+move[0] = 0
+move[1] = 0
+move[2] = 0
+move[3] = 0
+
+action0 = list()
+action1 = list()
+action2 = list()
+action3 = list()
+move_count = 0
 
 #%%
 observation = env.reset()
-for _ in tqdm(range(1000)):
+for _ in tqdm(range(maxSnakeLife)):
   env.render()
   action = env.action_space.sample() # your agent here (this takes random actions)
+
+  move_count += 1
+  move[action] += 1
+
   observation, reward, done, info = env.step(action)
+  totalreward += reward
 
   if done:
+    rewards.append(totalreward)
+    totalreward = 0
+    move_counts.append(move_count)
+    move_count = 0
+
+    action0.append(move[0])    
+    action1.append(move[1])    
+    action2.append(move[2])    
+    action3.append(move[3])
+    move.clear()
+    move[0] = 0
+    move[1] = 0
+    move[2] = 0
+    move[3] = 0
+
     observation = env.reset()
 env.close()
+
+data = {'reward': rewards,
+        'move_counts': move_counts,
+        'action0': action0,
+        'action1': action1,
+        'action2': action2,
+        'action3': action3}
+df = pd.DataFrame(data)
+data.clear()
+#%%
+df.head()
+#%%
+df.describe()
 
 #%%
