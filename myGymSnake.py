8f9c4121e782e3917c0946efd41eed9f56794739
Jacco Steegman <jacc.stee@gmail.com>
2019-10-22 00:59:21 +0200

    started testing my build model... not jet working

--- myGymSnake.py
+++ myGymSnake.py
@@ -22,6 +22,7 @@ backend.tensorflow_backend._get_available_gpus()
 from rl.agents.dqn import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
+from rl.core import Processor
 
 #%% prep envoriment
 maxSnakeLife = 1#10000
@@ -39,13 +40,19 @@ nb_acthions = env.action_space.n
 
 #%% build model
 model = Sequential()
-model.add(Conv2D(kernel_size=3,# midden plus zijkant
+model.add(Conv2D(kernel_size=(3,3),# midden plus zijkant
                  input_shape=(gameSize,gameSize,1),# 1 voor het aantal channels
+                 activation='relu',
+                 data_format='channels_first',
+                 filters=16))# filters elke kop 'directie; van 2 pixels 8 * 2 voor iets extra's?
+
+model.add(Conv2D(kernel_size=(3,3),# midden plus zijkant
+                 data_format='channels_first',
                  activation='relu',
                  filters=16))# filters elke kop 'directie; van 2 pixels 8 * 2 voor iets extra's?
 
 model.add(Flatten())
-model.add(Activation('relu'))
+#model.add(Activation('relu'))
 
 model.add(Dense(16))
 model.add(Activation('relu'))
@@ -55,6 +62,38 @@ model.add(Activation('linear'))
 
 print(model.summary())
 
+#%% build processor to for the input
+class my_input_processor(Processor):
+  def __init__(self):
+    print("using my_input_processor")
+  
+    def process_state_batch(self, batch):
+      returnList = list()
+      for state in batch:
+        returnList.append(state[0])
+      return returnList
+
+#%% initialize agent
+step_limit = 3000
+memory = SequentialMemory(limit=step_limit, window_length=1)
+policy = BoltzmannQPolicy()
+dqn = DQNAgent(
+               #processor=my_input_processor(),
+               model=model, 
+               nb_actions=nb_acthions,
+               memory=memory,
+               nb_steps_warmup=10,
+               target_model_update=1e-2, #how often to update the target model. t_m_u<1 = slowly update the model
+               policy=policy)
+
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+#%% train!
+#dqn.fit(env, nb_steps=step_limit, visualize=True, verbose=2, callbacks=[WandbCallback()])
+dqn.fit(env, nb_steps=step_limit, visualize=True, verbose=2)
+
+#%%
+dqn.test(env, nb_episodes=5, visualize=True)
 #%%
 observation = env.reset()
 for _ in tqdm(range(maxSnakeLife)):
